053407c7f7908a7f1d87a4ae1bf5d9c06c08735c
Carlos <carlos.oli.h23@gmail.com>
2026-02-02 15:13:59 -0300

    Os atributos e os métodos da super classe Funcionario foram adicionados.

--- src/entities/Funcionario.java
+++ src/entities/Funcionario.java
@@ -1,4 +1,40 @@
 package entities;
 
+import java.time.LocalTime;
+
 public abstract class Funcionario {
+    private static final LocalTime horarioDeAlmoco = LocalTime.of(1,0);
+    private static int ID = 1;
+
+    private final int id;
+    private String nomeFuncionario;
+    private int idadeFuncionario;
+
+    public Funcionario(String nomeFuncionario, int idadeFuncionario) {
+        this.id = ID;
+        this.nomeFuncionario = nomeFuncionario;
+        this.idadeFuncionario = idadeFuncionario;
+        ID++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario;
+    }
+
+    public int getIdadeFuncionario() {
+        return idadeFuncionario;
+    }
+    public void setIdadeFuncionario(int idadeFuncionario) {
+        this.idadeFuncionario = idadeFuncionario;
+    }
+
+    @Override
+    public abstract String toString();
 }
